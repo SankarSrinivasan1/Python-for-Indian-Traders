9420fd9181b429e8cfe5c17c84b7fa625bb2399c
Sankar Srinivasan <petra.srini@gmail.com>
2026-07-02 15:49:38 +0530

    Create settings.py for user configurations
    Added user adjustable settings for stock screener.

---
+++ PROJECTS/Indian-Stock-Screener/settings.py
@@ -0,0 +1,22 @@
+"""
+User Adjustable Settings
+"""
+
+AVAILABLE_SCREENERS = [
+    "RSI Oversold",
+    "RSI Overbought",
+    "Golden Cross",
+    "Supertrend Buy",
+    "ADX Trend",
+    "Bollinger Breakout"
+]
+
+DEFAULT_SYMBOL_SOURCE = "Nifty 50"
+
+SHOW_CHART = True
+
+ALLOW_EXPORT = True
+
+DEFAULT_SORT_COLUMN = "Symbol"
+
+DEFAULT_THEME = "Light"
