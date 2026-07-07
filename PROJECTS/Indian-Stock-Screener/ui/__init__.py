59baa9a8143dfd642d65c9b9ccbab1d700fde2b8
Sankar Srinivasan <petra.srini@gmail.com>
2026-07-07 12:37:02 +0530

    Create __init__.py for UI package documentation
    Added documentation for the UI package, including module list.

---
+++ PROJECTS/Indian-Stock-Screener/ui/__init__.py
@@ -0,0 +1,13 @@
+"""
+UI package for the Indian Stock Screener.
+
+Contains all Streamlit user interface components.
+
+Modules
+-------
+sidebar.py
+filters.py
+dashboard.py
+results_table.py
+charts.py
+"""
